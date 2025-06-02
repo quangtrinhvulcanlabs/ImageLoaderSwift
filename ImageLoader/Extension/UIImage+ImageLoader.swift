a165eb80869128eaf2c4fb00855cb30a3fa3a331
Quang Trinh <quangtrinh@vulcanlabs.co>
2025-06-02 11:13:42 +0700

    revert process function

--- ImageLoader/Extension/UIImage+ImageLoader.swift
+++ ImageLoader/Extension/UIImage+ImageLoader.swift
@@ -71,7 +71,7 @@ extension UIImage {
         return UIGraphicsGetImageFromCurrentImageContext()!
     }
 
-    static func process(data: Data) -> UIImage? {
+    internal static func process(data: Data) -> UIImage? {
         switch data.fileType {
         case .gif:
             guard let source = CGImageSourceCreateWithData(data as CFData, nil) else { return nil }
